7797f040a8e06ee2453ce1920083416bbf1a1ad8
Ahmed M Hamzah <ahmed.128hemzh@gmail.com>
2023-10-03 21:33:59 +0300

    fix registering a celery worker

---
+++ core/core/celery.py
@@ -0,0 +1,18 @@
+from celery import Celery
+import os
+import sys
+sys.path.append('/usr/src/app/ds-project/ds-core')  # Adjust the path as needed
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+app = Celery("core")  # The name should match your Django app name
+
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+
+@app.task
+def testCelery():
+    return
+
+
+app.autodiscover_tasks()
