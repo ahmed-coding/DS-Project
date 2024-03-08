33346cb5c1a3c8542bf5ba25b4d371b7f0b639cc
Ahmed M Hamzah <ahmed.128hemzh@gmail.com>
2024-03-08 22:56:21 +0300

    Update urls.py

--- core/apps/authentication/urls.py
+++ core/apps/authentication/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     path('login/', views.CustomAuthToken.as_view()),
     path('sginup/', views.ReigsterView.as_view()),
+    path('check-email/', views.ChechEmailValidateView.as_view(),
+         name='check_email_velidate'),
+    path('verify-email/', views. VerifyEmailView.as_view(), name='verify_email'),
+    path('send-verify-email/', views.SendVerifyEmailView.as_view(),
+         name='send_verify_email'),
     # path('logout/',)
 ]
